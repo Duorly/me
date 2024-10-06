2e3c8031c26c845cad82af3132095655fe30486e
Nebel MASS <52921005+Duorly@users.noreply.github.com>
2024-10-06 18:03:10 +0200

    Add packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-04-03',
+    modules: [
+        '@nuxt/devtools',
+        '@nuxt/ui',
+        '@pinia/nuxt',
+        'pinia-plugin-persistedstate',
+        '@nuxt/image',
+        '@nuxt/fonts'
+    ],
+    devtools: {
+        enabled: true
+    }
 })
