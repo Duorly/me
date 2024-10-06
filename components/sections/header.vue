57b8642dcb27ae173e5ac163a3717325b8cbb3b0
Nebel MASS <52921005+Duorly@users.noreply.github.com>
2024-10-07 00:29:43 +0200

    Add stack card

--- components/sections/header.vue
+++ components/sections/header.vue
@@ -8,6 +8,18 @@ const menu = [
   {name: 'Ma Stack', link: '#'},
   {name: 'Contact', link: '#'},
 ]
+
+
+const items = [{
+  key: 'frontend',
+  label: 'Frontend',
+}, {
+  key: 'backend',
+  label: 'Backend',
+}, {
+  key: 'devops',
+  label: 'Devops',
+}]
 </script>
 
 <template>
@@ -34,7 +46,7 @@ const menu = [
         </div>
       </nav>
       <div class="grid grid-cols-2 items-center container mx-auto mt-10">
-        <div class="flex space-y-8 flex-col bg-center bg-cover" >
+        <div class="flex space-y-8 flex-col bg-center bg-cover">
           <h1 class="text-white text-4xl font-custom">Hi ! I am</h1>
           <h1>
             <span class="font-extrabold text-cyan-500 font-title text-7xl">Duorli Nebel</span>
@@ -51,7 +63,8 @@ const menu = [
           </div>
 
           <div>
-            <button class="bg-cyan-600 border rounded-full text-white py-1 px-4 font-custom animate-bounce flex items-center space-x-2">
+            <button
+                class="bg-cyan-600 border rounded-full text-white py-1 px-4 font-custom animate-bounce flex items-center space-x-2">
               <Icon name="material-symbols:download-rounded" class="text-white"/>
               <span>
                  Télécharger mon CV
@@ -66,13 +79,68 @@ const menu = [
         </div>
       </div>
     </div>
+    <div class="bg-slate-900 h-screen p-20">
+      <div class="container mx-auto bg-slate-950 h-full rounded-lg p-10 space-y-10">
+        <div class="text-center">
+          <div>
+            <span class="text-cyan-500 font-custom">Mes spécialités</span>
+          </div>
+          <h1 class="text-white text-4xl font-custom">Ce que je fais</h1>
+          <p>
+            Ma mission est de vous aider à créer des sites web et des applications web modernes et performantes.
+          </p>
+        </div>
+        <UTabs :items="items" class="w-full">
+          <template #item="{ item }">
+            <div v-if="item.key === 'frontend'">
+              <div class="">
+                <div class="grid grid-cols-4 gap-4">
+                  <div v-for="i in 8" class="bg-slate-950 border-slate-900 border rounded-lg p-5">
+                    <div class="flex flex-col justify-center items-center text-center mx-auto">
+                      <img src="~/assets/images/stack/vuejs.svg" class="w-1/3" alt="">
+                      <h1 class="text-white
+              text-xl font-custom">JAVA</h1>
+                      <p class="text-white text-md">Framework javascript</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-else-if="item.key === 'backend'">
+              <div class="">
+                <div class="grid grid-cols-4 gap-4">
+                  <div v-for="i in 8" class="bg-slate-950 border-slate-900 border rounded-lg p-5">
+                    <div class="flex flex-col justify-center items-center text-center mx-auto">
+                      <img src="~/assets/images/stack/nuxt.svg" class="w-1/3" alt="">
+                      <h1 class="text-white
+              text-xl font-custom">JAVA</h1>
+                      <p class="text-white text-md">Framework javascript</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-else-if="item.key === 'devops'">
+              <div class="">
+                <div class="grid grid-cols-4 gap-4">
+                  <div v-for="i in 8" class="bg-slate-950 border-slate-900 border rounded-lg p-5">
+                    <div class="flex flex-col justify-center items-center text-center mx-auto">
+                      <img src="~/assets/images/stack/nuxt.svg" class="w-1/3" alt="">
+                      <h1 class="text-white
+              text-xl font-custom">JAVA</h1>
+                      <p class="text-white text-md">Framework javascript</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </template>
+        </UTabs>
+      </div>
+
+    </div>
+
   </div>
-
-  <div>
-    ss
-  </div>
-
-
 </template>
 
 <style scoped>
